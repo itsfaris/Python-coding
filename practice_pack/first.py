6000c0b256af5a6a46ede4d03d4f398e61c27989
faris hammaji <itsfaris@gmail.com>
2023-01-14 13:38:38 +0530

    add first.py and kids.py

---
+++ practice_pack/first.py
@@ -0,0 +1,4 @@
+print("Hello, I'am Shaik ziyad")
+n = int(input("enter number :"))
+for i in range(0,10):
+    print("{} * {} = {}".format(n,i,i*n))
---
+++ practice_pack/kids.py
@@ -0,0 +1,11 @@
+print ("enter property hight feet:")
+print ("enter property hight inches:")
+print ("enter property width feet:")
+print ("enter property width inches:")
+
+
+
+string_in = input("what is the house in sq feet:")
+square_feet = int(string_in)
+square_meter = square_feet/10.8
+print("the size is : {}".format(square_meter))
